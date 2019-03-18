7bfb05c6c1fd8a62811fc793a49303041f811b5e
Ed van Bruggen <edvb@uw.edu>
2019-03-17 23:09:11 -0700

    Fix crank hit box

--- src/main/java/ed/kappa/blocks/BlockCrank.java
+++ src/main/java/ed/kappa/blocks/BlockCrank.java
@@ -1,14 +1,12 @@
 package ed.kappa.blocks;
 
-import ed.kappa.Kappa;
 import ed.kappa.blocks.grinder.TileGrinder;
-import net.minecraft.block.Block;
-import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.tileentity.TileEntity;
@@ -20,11 +18,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
+import java.util.List;
+
 public class BlockCrank extends BlockBase {
     public static final PropertyBool POWERED = PropertyBool.create("powered");
     protected static final AxisAlignedBB CRANK_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, .125D, 1.0D);
 
-    // TODO fix crank hitbox, can't walk across it
     // TODO only place crank on top another block
     public BlockCrank() {
         super("crank", Material.ROCK);
@@ -58,7 +58,7 @@ public class BlockCrank extends BlockBase {
 	}
 
     @Override
-    public boolean isFullBlock(IBlockState state) {
+    public boolean isFullCube(IBlockState state) {
         return false;
     }
 
